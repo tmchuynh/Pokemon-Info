c46527f1069256e7c4b27401bb09908540820fc8
tmchuynh <tinamchuynh@gmail.com>
2023-03-07 13:01:47 -0800

    get pokemon details

---
+++ public/js/app.js
@@ -0,0 +1,8 @@
+$.getJSON("https://pokeapi.co/api/v2/pokemon?limit=1281", function(data) {
+    console.log(data);
+    for (var i = 0; i < data.results.length; i++) {
+        $.getJSON(data.results[i].url, function(results) {
+            console.log(results);
+        })
+    }
+});
