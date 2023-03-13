92305a1034b1ede724e670187e50234e6172480c
tmchuynh <tinamchuynh@gmail.com>
2023-03-13 08:56:52 -0700

    search for a specific id

--- public/js/app.js
+++ public/js/app.js
@@ -14,6 +14,19 @@ var results_;
 var left_arrow = document.querySelectorAll(".bi-arrow-left-square-fill");
 var right_arrow = document.querySelectorAll(".bi-arrow-right-square-fill");
 
+/* This is adding an event listener to the search bar, so that when the user presses the enter key, the
+value of the search bar will be set to the id_ variable, and then the getData() function will be
+called. */
+var search_bar = document.querySelector("#search_bar");
+search_bar.addEventListener("keyup", function (event) {
+    if (event.keyCode == 13) {
+        id_ = search_bar.value;
+        getData();
+    }
+    console.log(search_bar.value);
+});
+
+/* Adding an event listener to each element in the left_arrow array. */
 left_arrow.forEach(arrow => {
     arrow.addEventListener("click", () => {
         console.log(id_);
@@ -24,6 +37,7 @@ left_arrow.forEach(arrow => {
         getData();
     });
 });
+/* Adding an event listener to each element in the right_arrow array. */
 right_arrow.forEach(arrow => {
     arrow.addEventListener("click", () => {
         console.log(id_);
@@ -31,6 +45,8 @@ right_arrow.forEach(arrow => {
         getData();
     });
 });
+
+
 
 $(document).ready(function () {
     getData();
@@ -41,7 +57,10 @@ function getData() {
     $.getJSON("https://pokeapi.co/api/v2/pokemon/" + id_, function (data) {
         console.log(data);
         poke_name.innerHTML = data.name.toUpperCase();
-        order.innerHTML = "#" + data.order;
+        order.innerHTML = "#" + data.id;
+        /* This is checking if the pokemon has a dream world sprite, if it does it will use that, if
+        not it will check if it has a front sprite, if it does it will use that, if not it will use
+        a placeholder image. */
         if (data.sprites.other.dream_world.front_default != null) {
             main_image.src = data.sprites.other.dream_world.front_default;
 
@@ -53,22 +72,27 @@ function getData() {
             main_image.src = "https://imgs.search.brave.com/muGneTUNuFHdN8m6nvgTEdnD0geVMEu_VrxTdvEjtXI/rs:fit:300:300:1/g:ce/aHR0cHM6Ly9vcGVu/Y2xpcGFydC5vcmcv/aW1hZ2UvMzAwcHgv/c3ZnX3RvX3BuZy8x/OTQwNzcvUGxhY2Vo/b2xkZXIucG5n"
 
         }
+        /* This is creating a badge for each move that the pokemon has. */
         removeChildren(move_pills);
         for (var i = 0; i < data.moves.length; i++) {
             createBadgeElement(move_pills, data.moves[i].move.name, "text-bg-secondary")
         }
+        /* This is creating a badge for each type that the pokemon has. */
         removeChildren(type_pills);
         for (var i = 0; i < data.types.length; i++) {
             createBadgeElement(type_pills, data.types[i].type.name, "text-bg-success")
         }
+        /* This is creating a badge for each ability that the pokemon has. */
         removeChildren(ability_pills);
         for (var i = 0; i < data.abilities.length; i++) {
             createBadgeElement(ability_pills, data.abilities[i].ability.name, "text-bg-warning")
         }
+        /* This is creating a progress bar for each stat that the pokemon has. */
         removeChildren(progress_container);
         for (var i = 0; i < data.stats.length; i++) {
             createProgressBar(data.stats[i].base_stat, data.stats[i].stat.name)
         }
+        /* This is checking if the sprite has a front image, if it does it will display it. */
         removeChildren(sprites);
         for (var index in data.sprites) {
             if (index.includes("front") && data.sprites[index] != null) {
@@ -79,12 +103,21 @@ function getData() {
 }
 
 
+/**
+ * Remove all the children of a parent element.
+ * @param parent - The parent element that you want to remove all children from.
+ */
 function removeChildren(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
 }
 
+/**
+ * It creates a progress bar with the given value and name
+ * @param valuenow - The current value of the progress bar.
+ * @param name - The name of the progress bar.
+ */
 function createProgressBar(valuenow, name) {
     var p = document.createElement("p");
     p.innerHTML = name;
@@ -106,6 +139,13 @@ function createProgressBar(valuenow, name) {
     progress_container.appendChild(progress);
 }
 
+/**
+ * It creates a span element with the class "badge rounded-pill text-bg-primary" and the color class
+ * passed in as a parameter, and then sets the innerHTML of the span to the badge_text parameter.
+ * @param container - The element that the badge will be appended to.
+ * @param badge_text - The text that will be displayed on the badge.
+ * @param color - The color of the badge.
+ */
 function createBadgeElement(container, badge_text, color) {
     var badge = document.createElement("span");
     badge.classList.add("badge");
@@ -116,6 +156,12 @@ function createBadgeElement(container, badge_text, color) {
     container.appendChild(badge);
 }
 
+/**
+ * It creates an image element, sets its class, sets its source, sets its alt text, and then appends it
+ * to the sprites div
+ * @param name - The name of the Pokémon.
+ * @param spriteURL - The URL of the sprite image.
+ */
 function displaySprite(name, spriteURL) {
     var img = document.createElement("img");
     img.classList.add("img-fluid");
